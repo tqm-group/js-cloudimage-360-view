b8b176d50f69cab882a2b3e2b1f2c14ce0b161df
Ion <icaliman92@gmail.com>
2025-09-04 11:58:39 +0300

    don't use image width

--- src/utils/image-src/generate-low-preview-cdn-url.js
+++ src/utils/image-src/generate-low-preview-cdn-url.js
@@ -4,6 +4,10 @@ const generateLowPreviewCdnUrl = (cdnUrl) => {
   const cleanedCdnUrl = removeParamByRegex(cdnUrl, 'width');
   const separator = cleanedCdnUrl.includes('?') ? '&' : '?';
 
+  if (cleanedCdnUrl.includes('Signature')) {
+    return cleanedCdnUrl;
+  }
+
   return `${cleanedCdnUrl}${separator}width=${150 * devicePixelRatio}`;
 };
 
